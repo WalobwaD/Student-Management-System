37c402041c966740c2506f5825300b66bc434be1
WalobwaD <walobwadan@gmail.com>
2023-06-11 12:55:51 +0300

    Monitor

--- nodets/src/router/students.ts
+++ nodets/src/router/students.ts
@@ -7,10 +7,10 @@ export default (router: express.Router)=>{
     router.get('/', (req: express.Request, res: express.Response)=>{
         return res.status(200).json({ message: "Welcome to the Silabu's student management system api by Walobwa Dan"}).end()
     })
-    router.get('/students', getAllStudents)
-    router.get('/students/:id', getOneStudent)
-    router.patch('/students/update/:id',updateStudent)
-    router.delete('/students/suspend/:id',  suspend)
+    router.get('/students', isAuthenticated, getAllStudents)
+    router.get('/students/:id', isAuthenticated, getOneStudent)
+    router.patch('/students/update/:id',isAuthenticated, isMonitor,updateStudent)
+    router.delete('/students/suspend/:id',isAuthenticated, isMonitor,  suspend)
 }
 
 
