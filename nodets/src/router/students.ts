4a9635b7dc58663361a96b56eeade63b8289fc31
WalobwaD <walobwadan@gmail.com>
2023-06-11 11:14:24 +0300

    Added home

--- nodets/src/router/students.ts
+++ nodets/src/router/students.ts
@@ -3,6 +3,10 @@ import { getAllStudents, suspend, getOneStudent, updateStudent  } from "../contr
 import { isAuthenticated, isMonitor } from "../middlewares"
 import { get } from "lodash"
 export default (router: express.Router)=>{
+    //create home route
+    router.get('/', (req: express.Request, res: express.Response)=>{
+        return res.status(200).json({ message: "Welcome to the home page"}).end()
+    })
     router.get('/students', getAllStudents)
     router.get('/students/:id', getOneStudent)
     router.patch('/students/update/:id',updateStudent)
