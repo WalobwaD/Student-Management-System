c24722a1a1108ddb7cd6935a5bb33ca18109e7f3
WalobwaD <walobwadan@gmail.com>
2023-06-11 12:50:17 +0300

    Update message

--- nodets/src/router/students.ts
+++ nodets/src/router/students.ts
@@ -5,10 +5,12 @@ import { get } from "lodash"
 export default (router: express.Router)=>{
     //create home route
     router.get('/', (req: express.Request, res: express.Response)=>{
-        return res.status(200).json({ message: "Welcome to the home page"}).end()
+        return res.status(200).json({ message: "Welcome to the Silabu's student management system api by Walobwa Dan"}).end()
     })
     router.get('/students', getAllStudents)
     router.get('/students/:id', getOneStudent)
     router.patch('/students/update/:id',updateStudent)
     router.delete('/students/suspend/:id',  suspend)
 }
+
+
