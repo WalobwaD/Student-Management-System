d525aac9e0e89d3cb1ed02ecf5b3ce759a40209e
WalobwaD <walobwadan@gmail.com>
2023-06-12 07:58:30 +0300

    Added id

--- client/src/components/StudentsDashboard/UpdateStudent.jsx
+++ client/src/components/StudentsDashboard/UpdateStudent.jsx
@@ -78,7 +78,7 @@ const UpdateStudent = ()=>{
           <Typography component="h1" variant="h5">
             Update {name} {lastName}'s profile
           </Typography>
-          <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+          <Box component="form" noValidate onSubmit={handleSubmit(student._id)} sx={{ mt: 3 }}>
             <Grid container spacing={2}>
               <Grid item xs={12} sm={6}>
                 <TextField
